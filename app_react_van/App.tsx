f3ab104cfd0a5a8b18399bbd17dfe0c1b21f2d21
Vanessa <114593363+Vanpan58@users.noreply.github.com>
2024-03-31 00:12:16 -0500

    logo

--- app_react_van/App.tsx
+++ app_react_van/App.tsx
@@ -8,6 +8,16 @@ export default function App() {
         source={require('./assets/chef.jpg')}
         style={styles.imageBackground}
       />
+      <View style={styles.logoContainer}>
+        <Image
+        source={require('./assets/logo.png')}
+        style={styles.logoImage}
+        />
+        <Text style={styles.logoText}>FOOD APP</Text>
+      </View>
+
+      <View style={styles.form}></View>
+
     </View>
   );
 }
@@ -15,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
   },
   imageBackground: {
     width: '100%',
@@ -23,8 +33,23 @@ const styles = StyleSheet.create({
     opacity: 0.7,
     bottom: '30%',
   },
-
-  form: {
+  logoContainer:{
+    position:'absolute',
+    alignSelf: 'center',
+    top: '15%',
+  },
+  logoImage:{
+    width:100,
+    height:100
+  },
+  logoText:{
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+  form:{
     width: '100%',
     height: '40%',
     backgroundColor: 'white',
@@ -33,7 +58,6 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
   }
-
 });
 
 
